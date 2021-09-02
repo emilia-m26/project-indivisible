15452d7cd1a7ff56563dca24c3ff54d1a5379e87
Emilia <emilia.mena26@gmail.com>
2021-09-02 17:24:55 -0400

    details for Company table

--- indivisible-backend/db/migrate/20210902211643_create_companies.rb
+++ indivisible-backend/db/migrate/20210902211643_create_companies.rb
@@ -2,6 +2,16 @@ class CreateCompanies < ActiveRecord::Migration[6.0]
   def change
     create_table :companies do |t|
 
+      t.string :company_name
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.string :state
+      t.string :general_number
+      t.string :general_email
+      t.string :company_url
+      t.string :category
+
       t.timestamps
     end
   end
