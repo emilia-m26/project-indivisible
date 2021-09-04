3bc19f2f715c3688076348ccc9615b1b69118e8d
Emilia <emilia.mena26@gmail.com>
2021-09-03 21:29:29 -0400

    attributes for service table

--- indivisible-backend/db/migrate/20210904012654_create_services.rb
+++ indivisible-backend/db/migrate/20210904012654_create_services.rb
@@ -2,6 +2,15 @@ class CreateServices < ActiveRecord::Migration[6.0]
   def change
     create_table :services do |t|
 
+      t.string :servicer_name
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.string :state
+      t.string :contact_number
+      t.string :contact_email
+      t.string :service_url
+
       t.timestamps
     end
   end
