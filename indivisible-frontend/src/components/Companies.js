eea0230b9be593428c4ce936aa434bed080d6e7b
Emilia <emilia.mena26@gmail.com>
2021-09-08 17:53:17 -0400

    components for future pages

---
+++ indivisible-frontend/src/components/Companies.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Companies() {
+    return (
+        <div>
+            I AM THE COMPANIES PAGE
+        </div>
+    )
+}
+
---
+++ indivisible-frontend/src/components/ContactUs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ContactUs() {
+    return (
+        <div>
+            I am the contact us page
+        </div>
+    )
+}
