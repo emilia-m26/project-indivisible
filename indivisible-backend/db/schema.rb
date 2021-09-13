7264407197e6046d705743770e448f43391584e1
Emilia <emilia.mena26@gmail.com>
2021-09-13 16:09:51 -0400

    adding bcrypt

--- indivisible-backend/db/schema.rb
+++ indivisible-backend/db/schema.rb
@@ -10,57 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_012654) do
-
-  create_table "companies", force: :cascade do |t|
-    t.string "company_name"
-    t.string "address_1"
-    t.string "address_2"
-    t.string "city"
-    t.string "state"
-    t.string "general_number"
-    t.string "general_email"
-    t.string "company_url"
-    t.string "category"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "jobs", force: :cascade do |t|
-    t.string "job_title"
-    t.string "city"
-    t.string "state"
-    t.string "contact_email"
-    t.string "contact_number"
-    t.string "contact_person"
-    t.string "job_url"
-    t.integer "company_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "services", force: :cascade do |t|
-    t.string "servicer_name"
-    t.string "address_1"
-    t.string "address_2"
-    t.string "city"
-    t.string "state"
-    t.string "contact_number"
-    t.string "contact_email"
-    t.string "service_url"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
-    t.string "user_email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
