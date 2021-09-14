29a638d57ad457e1a9207b51326dd4c78f71fe58
Emilia <emilia.mena26@gmail.com>
2021-09-14 18:21:03 -0400

    adj route to include auth, adj home controller

--- indivisible-backend/config/routes.rb
+++ indivisible-backend/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
+  post 'auth_user' => 'authentication#authenticate_user'
   get 'home' => 'home#index'
+  devise_for :users
+  
   resources :services
   #resources :users
   resources :jobs
